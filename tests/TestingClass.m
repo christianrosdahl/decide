91a43699db383978f987cb1ad4f5bc737dfc8ab6
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-27 10:36:36 +0100

    Clarified intentions of tests for condition 1 (LIC1).

--- tests/TestingClass.m
+++ tests/TestingClass.m
@@ -7,7 +7,9 @@ classdef TestingClass < matlab.unittest.TestCase
     
     methods (Test)
         function testLIC1True(testCase)
-            % TESTLIC1True Checks if input is as expected (true) for lic1
+            % TESTLIC1True Checks that lic1 returns true when there are two
+            % consecutive data points that are a length greater than
+            % length1 apart.
             x = [1 2 5 6 7]; % x-coordinates of data points
             y = [1 2 5 6 7]; % y-coordinates of data points
             length1 = 2; % condition parameter
@@ -18,7 +20,9 @@ classdef TestingClass < matlab.unittest.TestCase
         end
         
         function testLIC1False(testCase)
-            % TESTLIC1False Checks if input is as expected (false) for lic1
+            % TESTLIC1False Checks that lic1 returns false when there are 
+            % not two consecutive data points that are a length greater 
+            % than length1 apart.
             x = [1 1.5 2 3 2.5]; % x-coordinates of data points
             y = [1 1.5 2 3 2.5]; % y-coordinates of data points
             length1 = 2; % condition parameter
