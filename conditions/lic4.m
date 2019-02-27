f5ded03010d3f4691d693f7c018c9e5ca5bd023c
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-27 21:18:59 +0100

    Implemented the function lic4.

--- conditions/lic4.m
+++ conditions/lic4.m
@@ -10,5 +10,23 @@ function fulfilled = lic4(x, y, area1)
 %   * y = y-coordinates of data points
 %   * area1 = condition parameter (area1 >= 0)
 
+if area1 < 0
+    error('area1 must satisfy area1 >= 0')
+end
+
 fulfilled = 0;
+
+for i = 1:length(x)-2 % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+1) y(i+1)];
+    p3 = [x(i+2) y(i+2)];
+    vec1 = p2 - p1
+    vec2 = p3 - p2
+    
+    area = abs(det([vec1; vec2]))/2
+    
+    if area > area1
+        fulfilled = 1;
+    end
+end
 end
