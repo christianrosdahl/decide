02c6233369e362f0a3e2bc46423525c6342ff1ef
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-27 22:43:12 +0100

    Implemented the function lic7.

--- conditions/lic7.m
+++ conditions/lic7.m
@@ -15,5 +15,45 @@ function fulfilled = lic7(x, y, n_pts, dist)
 %   * n_pts = condition parameter (3 <= n_pts <= numpoints)
 %   * dist = condition parameter (dist >= 0)
 
-fulfilled = 0;
+fulfilled = 0;  
+
+if n_pts < 3 || n_pts > length(x)
+    error('n_pts must satisfy 3 <= n_pts <= numpoints')
+end
+if dist < 0
+    error('dist must satisfy dist >= 0')
+end
+
+largestDist = 0;
+for i = 1:length(x)-n_pts+1 % Iterate through data points
+    x1 = x(i);
+    y1 = y(i);
+    x2 = x(i+n_pts-1);
+    y2 = y(i+n_pts-1);
+    
+    if norm([x1 y1] - [x2 y2]) > 0
+        m = y1 - x1*(y2-y1)/(x2-x1); % Intersection of line with y-axis.
+        lineVector = [x2 y2] - [x1 y1];
+        lineVector = lineVector/norm(lineVector); % Normalised line vector.
+        
+        for j = i+1:i+n_pts-2
+            vec = [x(j) y(j)-m]; % Point vector displaced to line.
+            projection = dot(vec,lineVector)*lineVector; % Proj. of vec on line.
+            distVector = vec - projection;
+            dist = norm(distVector); % Distance between line and point.
+            largestDist = max(largestDist, dist);
+        end
+    else % If first and last point coincide
+        for j = i+1:i+n_pts-2
+            vec = [x(j) y(j)];
+            dist = norm([x1 y1] - vec);
+            largestDist = max(largestDist, dist);
+        end
+    end
+end
+
+if largestDist > dist
+    fulfilled = 1;
+end
+
 end
