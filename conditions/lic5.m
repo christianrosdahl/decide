99c062f6ad4d8a684cc9a959bd96a94a0875e4fe
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-27 21:49:33 +0100

    Implemented function lic5 (including help function quadrant).

--- conditions/lic5.m
+++ conditions/lic5.m
@@ -13,4 +13,17 @@ function fulfilled = lic5(x, y, q_pts, quads)
 %   * quads = condition parameter (1 <= quads <= 3)
 
 fulfilled = 0;
+
+for i = 1:length(x)-q_pts+1 % Iterate through data points
+    xx = x(i:i+q_pts-1);
+    yy = y(i:i+q_pts-1);
+    quadrants = zeros(1,q_pts);
+    for j = 1:q_pts
+        quadrants(j) = quadrant(xx(j),yy(j));
+    end
+    nbr_quads = length(unique(quadrants));
+    if nbr_quads > quads
+        fulfilled = 1;
+    end
+end
 end
---
+++ conditions/quadrant.m
@@ -0,0 +1,13 @@
+function q = quadrant(x,y)
+% QUADRANT Returns number q of the quadrant in which the point (x,y) is.
+if x >= 0 && y >= 0
+    q = 1;
+elseif x < 0 && y >= 0
+    q = 2;
+elseif x <= 0 && y < 0
+    q = 3;
+else
+    q = 4;
+end
+end
+
