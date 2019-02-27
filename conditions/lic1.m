9413026978cc24761ea03fc9ac59c8ed6342c6fa
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-27 19:13:53 +0100

    Implemented the function lic1.

--- conditions/lic1.m
+++ conditions/lic1.m
@@ -9,5 +9,15 @@ function fulfilled = lic1(x, y, length1)
 %   * y = y-coordinates of data points
 %   * length1 = condition parameter (length1 >= 0)
 
+if length1 < 0
+    error('length1 must satisfy length1 >= 0')
+end
+
 fulfilled = 0;
+
+for i = 1:length(x)-1 % Iterate through data points
+    if norm([x(i+1) y(i+1)] - [x(i) y(i)]) > length1
+        fulfilled = 1;
+    end
+end
 end
