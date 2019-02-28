ee54a7de2e2edfe6268d9984ebd77236e2ee69e9
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 09:53:53 +0100

    Implemented the function lic8.

--- conditions/lic8.m
+++ conditions/lic8.m
@@ -12,5 +12,22 @@ function fulfilled = lic8(x, y, k_pts, length1)
 %   * k_pts = condition parameter (1 <= k_pts <= numpoints - 2)
 %   * length1 = condition parameter (lenght1 >= 0)
 
+if length1 < 0
+    error('length1 must satisfy length1 >= 0')
+end
+if k_pts < 1 || k_pts > length(x) - 2
+    error('k_pts must satisfy 1 <= k_pts <= numpoints - 2')
+end
+
 fulfilled = 0;
+
+for i = 1:length(x)-k_pts-1 % Iterate through data points
+    if norm([x(i+k_pts+1) y(i+k_pts+1)] - [x(i) y(i)]) > length1
+        fulfilled = 1;
+    end
+end
+
+if length(x) < 3
+    fulfilled = 0;
+end
 end
