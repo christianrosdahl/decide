2e5eb198ccfdc2c4774db7f0958c16e65f2d4972
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 10:38:58 +0100

    Implemented the function lic13.

--- conditions/lic13.m
+++ conditions/lic13.m
@@ -17,5 +17,38 @@ function fulfilled = lic13(x, y, k_pts, length1, length2)
 %   * length1 = condition parameter (length1 >= 0)
 %   * length2 = condition parameter (length2 >= 0)
 
+if length1 < 0
+    error('length1 must satisfy length1 >= 0')
+end
+if length2 < 0
+    error('length2 must satisfy length2 >= 0')
+end
+if k_pts < 1 || k_pts > length(x) - 2
+    error('k_pts must satisfy 1 <= k_pts <= numpoints - 2')
+end
+
 fulfilled = 0;
+condition1 = 0;
+condition2 = 0;
+
+for i = 1:length(x)-1-k_pts % Iterate through data points
+    if norm([x(i+k_pts+1) y(i+k_pts+1)] - [x(i) y(i)]) > length1
+        condition1 = 1;
+    end
+end
+
+for i = 1:length(x)-1-k_pts % Iterate through data points
+    if norm([x(i+k_pts+1) y(i+k_pts+1)] - [x(i) y(i)]) < length2
+        condition2 = 1;
+    end
+end
+
+if condition1 && condition2
+    fulfilled = 1;
+end
+
+% Not fulfilled when numpoints < 3.
+if length(x) < 3
+    fulfilled = 0;
+end
 end
