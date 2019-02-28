2beb783fbc2657abbe5f5a9911ac61e6b6b13527
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 10:26:17 +0100

    Implemented the function lic11.

--- conditions/lic11.m
+++ conditions/lic11.m
@@ -16,5 +16,37 @@ function fulfilled = lic11(x, y, e_pts, f_pts, area1)
 %       (f_pts >= 1, e_pts + f_pts <= numpoints - 3)
 %   * area1 = condition parameter (area1 >= 0)
 
+if area1 < 0
+    error('area1 must satisfy area1 >= 0')
+end
+if e_pts < 0
+    error('e_pts must satisfy e_pts >= 0')
+end
+if f_pts < 0
+    error('f_pts must satisfy f_pts >= 0')
+end
+if e_pts + f_pts > length(x) - 3
+    error('e_pts and f_pts must satisfy e_pts + f_pts <= numpoints - 3')
+end
+
 fulfilled = 0;
+
+for i = 1:length(x)-2-e_pts-f_pts % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+e_pts+1) y(i+e_pts+1)];
+    p3 = [x(i+e_pts+f_pts+2) y(i+e_pts+f_pts+2)];
+    vec1 = p2 - p1
+    vec2 = p3 - p2
+    
+    area = abs(det([vec1; vec2]))/2
+    
+    if area > area1
+        fulfilled = 1;
+    end
+end
+
+% Not fulfilled when numpoints < 5.
+if length(x) < 5
+    fulfilled = 0;
+end
 end
