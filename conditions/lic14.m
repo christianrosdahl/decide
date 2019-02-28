677c561aca6081a70b6e625be5a1d9d640a379c3
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 10:51:01 +0100

    Implemented the function lic14.m

--- conditions/lic14.m
+++ conditions/lic14.m
@@ -21,5 +21,133 @@ function fulfilled = lic14(x, y, a_pts, b_pts, radius1, radius2)
 %   * radius1 = condition parameter (radius1 >= 0)
 %   * radius2 = condition parameter (radius2 >= 0)
 
+if radius1 < 0
+    error('radius1 must satisfy radius1 >= 0')
+end
+if radius2 < 0
+    error('radius2 must satisfy radius2 >= 0')
+end
+if a_pts < 0
+    error('a_pts must satisfy a_pts >= 0')
+end
+if b_pts < 0
+    error('b_pts must satisfy b_pts >= 0')
+end
+if a_pts + b_pts > length(x) - 3
+    error('a_pts and b_pts must satisfy a_pts + b_pts <= numpoints - 3')
+end
+
 fulfilled = 0;
+condition1 = 0;
+condition2 = 0;
+
+%% Condition1:
+
+% The smallest circle containing three points has either all points on
+% the boundary or has two points on the boundary defining the diameter of
+% the circle (see "Smallest-circle problem").
+
+for i = 1:length(x)-2-a_pts-b_pts % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+a_pts+1) y(i+a_pts+1)];
+    p3 = [x(i+a_pts+b_pts+2) y(i+a_pts+b_pts+2)];
+    
+    smallestRadius = inf;
+    
+    % Test if smallest circle is defined by p1 and p2 (and contains p3):
+    c = (p1 + p2)/2; % Center point of circle
+    r = norm(p2 - p1)/2; % Radius of circle
+    if (p3(1) - c(1))^2 + (p3(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle is defined by p1 and p3 (and contains p2):
+    c = (p1 + p3)/2; % Center point of circle
+    r = norm(p3 - p1)/2; % Radius of circle
+    if (p2(1) - c(1))^2 + (p2(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle is defined by p2 and p3 (and contains p1):
+    c = (p2 + p3)/2; % Center point of circle
+    r = norm(p3 - p2)/2; % Radius of circle
+    if (p1(1) - c(1))^2 + (p1(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle has all points on boundary:
+    A = [2*(p2(1)-p1(1)) 2*(p2(2)-p1(2)); ...
+            2*(p3(1)-p1(1)) 2*(p3(2)-p1(2))];
+    if (det(A) > 0)
+        b = [p2(1)^2 - p1(1)^2 + p2(2)^2 - p1(2)^2; ...
+            p3(1)^2 - p1(1)^2 + p3(2)^2 - p1(2)^2];
+        c = A\b;
+        r = sqrt((p1(1) - c(1))^2 + (p1(2) - c(2))^2);
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    if smallestRadius > radius1
+        condition1 = 1;
+    end
+end
+
+%% Condition2:
+
+% The smallest circle containing three points has either all points on
+% the boundary or has two points on the boundary defining the diameter of
+% the circle (see "Smallest-circle problem").
+
+for i = 1:length(x)-2-a_pts-b_pts % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+a_pts+1) y(i+a_pts+1)];
+    p3 = [x(i+a_pts+b_pts+2) y(i+a_pts+b_pts+2)];
+    
+    smallestRadius = inf;
+    
+    % Test if smallest circle is defined by p1 and p2 (and contains p3):
+    c = (p1 + p2)/2; % Center point of circle
+    r = norm(p2 - p1)/2; % Radius of circle
+    if (p3(1) - c(1))^2 + (p3(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle is defined by p1 and p3 (and contains p2):
+    c = (p1 + p3)/2; % Center point of circle
+    r = norm(p3 - p1)/2; % Radius of circle
+    if (p2(1) - c(1))^2 + (p2(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle is defined by p2 and p3 (and contains p1):
+    c = (p2 + p3)/2; % Center point of circle
+    r = norm(p3 - p2)/2; % Radius of circle
+    if (p1(1) - c(1))^2 + (p1(2) - c(2))^2 <= r^2
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    % Test if smallest circle has all points on boundary:
+    A = [2*(p2(1)-p1(1)) 2*(p2(2)-p1(2)); ...
+            2*(p3(1)-p1(1)) 2*(p3(2)-p1(2))];
+    if (det(A) > 0)
+        b = [p2(1)^2 - p1(1)^2 + p2(2)^2 - p1(2)^2; ...
+            p3(1)^2 - p1(1)^2 + p3(2)^2 - p1(2)^2];
+        c = A\b;
+        r = sqrt((p1(1) - c(1))^2 + (p1(2) - c(2))^2);
+        smallestRadius = min(smallestRadius,r);
+    end
+    
+    if smallestRadius <= radius2
+        condition2 = 1;
+    end
+end
+
+%%
+if condition1 && condition2
+    fulfilled = 1;
+end
+
+% Not fulfilled when numpoints < 5.
+if length(x) < 5
+    fulfilled = 0;
+end
 end
