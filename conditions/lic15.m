a8cf1336386b3df8acec278a34ce6ab3f1d029b2
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 10:56:26 +0100

    Implemented the function lic15.

--- conditions/lic15.m
+++ conditions/lic15.m
@@ -21,5 +21,63 @@ function fulfilled = lic15(x, y, e_pts, f_pts, area1, area2)
 %   * area1 = condition parameter (area1 >= 0)
 %   * area2 = condition parameter (area2 >= 0)
 
+if area1 < 0
+    error('area1 must satisfy area1 >= 0')
+end
+if area2 < 0
+    error('area2 must satisfy area2 >= 0')
+end
+if e_pts < 0
+    error('e_pts must satisfy e_pts >= 0')
+end
+if f_pts < 0
+    error('f_pts must satisfy f_pts >= 0')
+end
+if e_pts + f_pts > length(x) - 3
+    error('e_pts and f_pts must satisfy e_pts + f_pts <= numpoints - 3')
+end
+
 fulfilled = 0;
+condition1 = 0;
+condition2 = 0;
+
+%% Condition 1:
+for i = 1:length(x)-2-e_pts-f_pts % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+e_pts+1) y(i+e_pts+1)];
+    p3 = [x(i+e_pts+f_pts+2) y(i+e_pts+f_pts+2)];
+    vec1 = p2 - p1;
+    vec2 = p3 - p2;
+    
+    area = abs(det([vec1; vec2]))/2;
+    
+    if area > area1
+        condition1 = 1;
+    end
+end
+
+%% Condition2:
+for i = 1:length(x)-2-e_pts-f_pts % Iterate through data points
+    p1 = [x(i) y(i)];
+    p2 = [x(i+e_pts+1) y(i+e_pts+1)];
+    p3 = [x(i+e_pts+f_pts+2) y(i+e_pts+f_pts+2)];
+    vec1 = p2 - p1;
+    vec2 = p3 - p2;
+    
+    area = abs(det([vec1; vec2]))/2;
+    
+    if area < area2
+        condition2 = 1;
+    end
+end
+
+%%
+if condition1 && condition2
+    fulfilled = 1;
+end
+
+% Not fulfilled when numpoints < 5.
+if length(x) < 5
+    fulfilled = 0;
+end
 end
