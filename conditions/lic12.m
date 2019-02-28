16b429ccf97c6ba22c80535d570f7ea02e529adb
Christian Rosdahl <christian.rosdahl@control.lth.se>
2019-02-28 10:30:21 +0100

    Implemented the function lic12.

--- conditions/lic12.m
+++ conditions/lic12.m
@@ -11,5 +11,20 @@ function fulfilled = lic12(x, y, g_pts)
 %   * y = y-coordinates of data points
 %   * g_pts = condition parameter (1 <= g_pts <= numpoints - 2)
 
+if g_pts < 1 || g_pts > length(x) - 2
+    error('g_pts must satisfy 1 <= g_pts <= numpoints - 2')
+end
+
 fulfilled = 0;
+
+for i = 1:length(x)-1-g_pts % Iterate through data points
+    if x(i+g_pts+1) - x(i) < 0
+        fulfilled = 1;
+    end
+end
+
+% Not fulfilled when numpoints < 3.
+if length(x) < 3
+    fulfilled = 0;
+end
 end
